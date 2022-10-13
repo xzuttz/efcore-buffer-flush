69bfe71c04f8408636d5dca0cc9255bd29afead0
Kenan Imamovic <ximam@orsted.com>
2022-10-13 09:46:26 +0200

    Fixed bug and removed unsued code

--- src/SaveChangesMaybe/Extensions/BulkUpdateMaybeExtensions.cs
+++ src/SaveChangesMaybe/Extensions/BulkUpdateMaybeExtensions.cs
@@ -6,12 +6,9 @@ namespace SaveChangesMaybe.Extensions
 {
     public static class BulkUpdateMaybeExtensions
     {
-        public delegate void Print();
-
         public static Task BulkUpdateMaybeAsync<T>(this DbSet<T> dbSet, List<T> entities, int batchSize, Action<BulkOperation<T>>? options = null) where T : class
         {
-            dbSet.BulkMerge(null, null);
-            dbSet.SaveChangesMaybe(entities, batchSize, SaveChangesMaybeOperationType.BulkUpdateAsync, options);
+            dbSet.SaveChangesMaybe(entities, batchSize, SaveChangesMaybeOperationType.BulkUpdate, options);
             return Task.FromResult(Task.CompletedTask);
         }
 
