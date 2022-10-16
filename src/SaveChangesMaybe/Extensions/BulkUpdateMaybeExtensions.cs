a0a912ea93646359c505185e3afd78cd414c850e
Kenan Imamovic <ximam@orsted.com>
2022-10-16 10:06:23 +0200

    Added BulkUpdate to DbContext

--- src/SaveChangesMaybe/Extensions/BulkUpdateMaybeExtensions.cs
+++ src/SaveChangesMaybe/Extensions/BulkUpdateMaybeExtensions.cs
@@ -7,6 +7,48 @@ namespace SaveChangesMaybe.Extensions
 {
     public static class BulkUpdateMaybeExtensions
     {
+
+        // DbContext
+
+        public static async Task BulkUpdateMaybeAsync<T>(this DbContext dbContext, List<T> entities, int batchSize, CancellationToken cancellationToken,
+            Action<BulkOperation<T>>? options = null) where T : class
+        {
+            /// TODO: async all the way down
+            await Task.Run(() =>
+            {
+                BulkUpdateMaybe(dbContext, entities, batchSize, options);
+            }, cancellationToken).ConfigureAwait(false);
+        }
+
+        public static void BulkUpdateMaybe<T>(this DbContext dbContext, List<T> entities, int batchSize, Action<BulkOperation<T>>? options = null) where T : class
+        {
+            var callback = new Action<List<T>>(list =>
+            {
+                if (options is null)
+                {
+                    dbContext.BulkMerge(list);
+                }
+                else
+                {
+                    dbContext.BulkMerge(list, options);
+                }
+            });
+
+            var wrapper = new SaveChangesMaybeWrapper<T>
+            {
+                SaveChangesCallback = callback,
+                BatchSize = batchSize,
+                Entities = entities,
+                OperationType = SaveChangesMaybeOperationType.BulkMerge,
+                Options = options
+            };
+
+            SaveChangesMaybeBufferHelper.SaveChangesMaybe(wrapper);
+        }
+
+
+        // DbSet
+
         public static async Task BulkUpdateMaybeAsync<T>(this DbSet<T> dbSet, List<T> entities, int batchSize, CancellationToken cancellationToken, Action<BulkOperation<T>>? options = null) where T : class
         {
             /// TODO: async all the way down
